5ec130ec69f98fc0fe9e96e541986847d2a2ad58
Adrian Eguez <eadepto@hotmail.com>
2015-11-09 07:53:19 -0500

    primer commit

---
+++ QuizApp/api/models/Usuarios.js
@@ -0,0 +1,27 @@
+/**
+* Usuarios.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+      nombre: {
+            type: 'string'
+        },
+      apellido: {
+            type: 'string'
+        },
+      correo: {
+            type: 'string'
+        },
+      password: {
+            type: 'string',
+            defaultsTo: 123456 
+        }
+
+  }
+};
+
